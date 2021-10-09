658f0183ca5a0e767cbaf0f4077f6be0d53866a8
jasminbhesaniyajb <jasminbhesaniyajb@gmail.com>
2021-10-09 18:02:08 +0530

    [ADD] home page

---
+++ assets/js/custom.js
@@ -0,0 +1,79 @@
+$(".owl-carousel2").owlCarousel({
+  loop: true,
+  margin: 40,
+  nav: false,
+  autoplay: true,
+  speed: 2000,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+//   secound slider
+
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false,
+        },
+        1000: {
+          items: 3,
+          nav: true,
+        },
+      },
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
+
+//   faq
+
+const toggles = document.querySelectorAll(".faq-toggles");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
+
+
+// smooth scroll
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function() {
+    var href = $.attr(this, 'href');
+
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+
+    return false;
+});
